b4fd05823ec4e5d62e5370f4300af091beff1069
Rajan Patel0 <rajanpatel.ias@gmail.com>
2025-07-31 18:30:40 +0530

    heap learning

--- Heap/start.cpp
+++ Heap/start.cpp
@@ -6,14 +6,19 @@ using namespace std;
 class Heap{
     public:
     int arr[100];
-    int size=0;
+    int size;
+
+    Heap(){
+        arr[0]=-1;
+        size=0;
+    }
 
     void insert(int val){
         size=size+1;
         int index = size;
         arr[index] = val;
 
-        while(index>1){
+        while(index>1){     //0(log(n))
             int parent = index/2;
 
             if(arr[parent]<arr[index]){
@@ -24,11 +29,41 @@ class Heap{
             }
         }
     }
+
     void print(){
         for(int i=1; i<=size; i++){
             cout<<arr[i]<<" ";
         }cout<<endl;
     }
+
+    void deleteFromHeap(){
+        if(size==0){
+            cout<<"Nothing To Delete"<<endl;
+            return;
+        }
+
+        arr[1]=arr[size]; //last ko root(starting pehle) mei daal diya
+        size--; //removing last element jo ki swapped root hoga
+
+        //Take curr root to its correct position
+        int i=1;
+        while(i<=size){
+            int leftIdx=2*i;
+            int rightIdx=2*i+1;
+
+            if(leftIdx<size && arr[i]<arr[leftIdx]){
+                swap(arr[i], arr[leftIdx]);
+                i=leftIdx;
+            }else if(rightIdx<size && arr[i]< arr[rightIdx]){
+                swap(arr[i], arr[rightIdx]);
+                i=rightIdx;
+            }else{
+                return;
+            }
+        }
+    }
+
+    
 };
 
 
@@ -44,6 +79,9 @@ int main(){
     
     h.print();  //for printing output
 
+    h.deleteFromHeap();
+
+    h.print();
 
     return 0;
 }
